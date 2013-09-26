735368becdb4d7986db980dadba676b052c44057
Jean pasqualini <jpasqualini75@gmail.com>
2013-09-26 17:37:56 +0200

    Add gregwar image bundle for gregwar image component to zf

---
+++ src/bundles/gregwarImageBundle/ImageHandler.php
@@ -0,0 +1,72 @@
+<?php
+
+use Gregwar\ImageBundle\Image;
+
+/**
+ * Image manipulation class
+ *
+ * @author Gregwar <g.passault@gmail.com>
+ */
+class ImageHandler extends Image
+{
+    protected $fileCallback   = null;
+    protected $throwException = null;
+
+    /**
+     * @param null $originalFile
+     * @param null $width
+     * @param null $height
+     * @param bool $throwException
+     */
+    public function __construct($originalFile = null, $width = null, $height = null, $throwException = null)
+    {
+        $this->throwException = $throwException;
+        parent::__construct($originalFile, $width, $height);
+    }
+
+
+    /**
+     * Defines the callback to call to compute the new filename
+     */
+    public function setFileCallback($file)
+    {
+        $this->fileCallback = $file;
+    }
+
+    /**
+     * When processing the filename, call the callback
+     */
+    protected function getFilename($filename)
+    {
+        $callback = $this->fileCallback;
+
+        if (null === $callback || substr($filename, 0, 1) == '/')
+            return $filename;
+
+        return $callback($filename);
+    }
+
+    public function save($file, $type = 'guess', $quality = 80)
+    {
+        try{
+            return parent::save($file, $type, $quality);
+        } catch(\Exception $e) {
+            if($this->throwException) {
+                throw $e;
+            }
+            return false;
+        }
+    }
+
+    public function __toString()
+    {
+        try{
+            return parent::__toString();
+        } catch(\Exception $e) {
+            if($this->throwException) {
+                return $e->getMessage();
+            }
+            return '';
+        }
+    }
+}
---
+++ src/bundles/gregwarImageBundle/service/gregwarImage.php
@@ -0,0 +1,74 @@
+<?php
+/**
+ * Created by JetBrains PhpStorm.
+ * User: adibox
+ * Date: 26/09/13
+ * Time: 16:46
+ * To change this template use File | Settings | File Templates.
+ */
+
+namespace service;
+
+
+class gregwarImage extends \Twig_Extension {
+    private $options = array();
+
+    private $container;
+
+    public function __construct(array $options, \serviceContainer $container)
+    {
+        $this->options = $options;
+
+        $this->container = $container;
+    }
+
+    public function getName()
+    {
+        return "gregwarimage.twig.extension";
+    }
+
+    public function getFunctions()
+    {
+        return array(
+            'image' => new \Twig_Function_Method($this, 'image', array('is_safe' => array('html'))),
+            'new_image' => new \Twig_Function_Method($this, 'newImage', array('is_safe' => array('html')))
+        );
+    }
+
+    public function image($path)
+    {
+        return $this-open($path);
+    }
+
+    public function newImage($width, $height)
+    {
+        return $this->create($width, $height);
+    }
+
+    public function open($file)
+    {
+        return $this->createInstance($file);
+    }
+
+    public function create($w, $h)
+    {
+        return $this->createInstance(null, $w, $h);
+    }
+
+    private function createInstance($file, $w = null, $h = null)
+    {
+
+        $handler_class = $this->options["handler_class"];
+
+        $image = new $handler_class($file, $w, $h, null);
+
+        $image->setCacheDir($this->options["cache_dir"]);
+
+        $image->setFileCallback(function($file)
+        {
+           return $file;
+        });
+
+        return $image;
+    }
+}
