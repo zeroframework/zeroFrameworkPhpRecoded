105b86dc990d1d5d3dd33aab6275bfcf8c38f64a
onetest <jpasqualini75@gmail.com>
2014-01-20 17:27:32 +0100

    active cache

--- src/bundles/symfonyHttpCacheBundle/symfonyHttpCacheBundle.php
+++ src/bundles/symfonyHttpCacheBundle/symfonyHttpCacheBundle.php
@@ -1,4 +1,10 @@
 <?php
+use Symfony\Component\HttpKernel\HttpCache\HttpCache;
+
+use Symfony\Component\HttpKernel\HttpCache\Esi;
+
+use Symfony\Component\HttpKernel\HttpCache\Store;
+
 class symfonyHttpCacheBundle
 {
     public static function register($app)
@@ -6,7 +12,7 @@ class symfonyHttpCacheBundle
         $app = $app->getServiceContainer();
 
         $app['http_cache'] = $app->share(function ($app) {
-            return new HttpCache($app, $app['http_cache.store'], $app['http_cache.esi'], $app['http_cache.options']);
+            return new HttpCache($app['kernel'], $app['http_cache.store'], $app['http_cache.esi'], $app['http_cache.options']);
         });
 
         $app['http_cache.esi'] = $app->share(function ($app) {
