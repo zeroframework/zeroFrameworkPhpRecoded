9cab31d6a519cc6f6d18f0670737c6f9217095e0
onetest <jpasqualini75@gmail.com>
2014-04-14 16:18:53 +0200

    Add cache to configuration file

--- lib/conf.php
+++ lib/conf.php
@@ -7,24 +7,62 @@ class conf {
 
     }
 
+    public function isCache()
+    {
+        static $apc;
+
+        if($apc === null) $apc = (PHP_SAPI != "cli" && extension_loaded("apc"));
+
+        return $apc;
+    }
+
     public function loadConfigurationFile($name, $directory)
     {
         $pathWithoutExtension = $directory.DIRECTORY_SEPARATOR.$name;
 
-        if(file_exists($pathWithoutExtension.".ini"))
+        $key = md5("framework_configuration_".$directory."_".$name);
+
+        if(!$this->isCache() || !apc_exists($key))
         {
-            $vars = parse_ini_file($pathWithoutExtension.".ini", true);
-        }
-        elseif(file_exists($pathWithoutExtension.".json"))
-        {
-            $vars = json_decode(file_get_contents($pathWithoutExtension.".json"), true);
-        }
-        elseif(file_exists($pathWithoutExtension.".php"))
-        {
-            $vars = require_once($pathWithoutExtension.".php");
+            if(file_exists($pathWithoutExtension.".ini"))
+            {
+                $vars = parse_ini_file($pathWithoutExtension.".ini", true);
+            }
+            elseif(file_exists($pathWithoutExtension.".json"))
+            {
+                $vars = json_decode(file_get_contents($pathWithoutExtension.".json"), true);
+            }
+            elseif(file_exists($pathWithoutExtension.".php"))
+            {
+                $vars = require_once($pathWithoutExtension.".php");
+            }
+
+            if(!isset($vars)) {
+                throw new \Exception("configuration doesn't load $directory : $name !");
+            }
+
+            if($this->isCache()) 
+            {
+                apc_store($key, $vars);
+            }
         }
 
-        if(!isset($vars)) throw new \Exception("configuration doesn't load $directory : $name !");
+        if($this->isCache() && apc_exists($key))
+        {
+            $vars = apc_fetch($key);
+        }
+
+        if(array_key_exists("imports", $vars))
+        {
+            $dataImporteds = array();
+
+            foreach($vars["imports"] as $file)
+            {
+                $dataImporteds = array_merge($dataImporteds, $this->loadConfigurationFile($file, $directory));
+            }
+
+            $vars = array_merge($dataImporteds, $vars);
+        }
 
         return $vars;
     }
